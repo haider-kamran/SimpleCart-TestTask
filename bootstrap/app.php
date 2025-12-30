dda0a5ea67b0e504fe61bbd6f736d24148c09f93
Kamran Haider <kamrankhosa40@gmail.com>
2025-12-30 21:08:30 +0500

    Add daily sales report job to application schedule

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Console\Scheduling\Schedule;
+use App\Jobs\DailySalesReportJob;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,6 +23,10 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->job(new DailySalesReportJob)
+            ->dailyAt('20:00');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
